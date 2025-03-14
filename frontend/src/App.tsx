e179e5f948745df111ef2a564a4a0a1b3a524a0a
James Seelos <97702507+seelosjames@users.noreply.github.com>
2025-03-14 16:00:51 -0600

    Setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,31 @@
-import './App.css'
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 
 function App() {
 
   return (
-    <>
-      <h1>Slam Stats</h1>
-    </>
-  )
+		<Router>
+			<div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
+				<h1 className="text-4xl font-bold mb-6">Welcome to My Django/React Template</h1>
+				<div className="flex flex-col gap-4">
+					<Link to="/signin">
+						<button className="w-48">Sign In</button>
+					</Link>
+					<Link to="/create-account">
+						<button className="w-48">Create Account</button>
+					</Link>
+					<Link to="/user-info">
+						<button className="w-48">User Information</button>
+					</Link>
+					<button className="w-48 bg-red-500 hover:bg-red-600">Logout</button>
+				</div>
+			</div>
+			<Routes>
+				<Route path="/signin" element={<div>Sign In Page</div>} />
+				<Route path="/create-account" element={<div>Create Account Page</div>} />
+				<Route path="/user-info" element={<div>User Information Page</div>} />
+			</Routes>
+		</Router>
+	);
 }
 
 export default App

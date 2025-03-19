b0f2e708794a162b5f1e75b74336ea6a79f54cb2
James Seelos <97702507+seelosjames@users.noreply.github.com>
2025-03-19 15:35:01 -0600

    Change design

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -27,6 +27,11 @@ function App() {
 
 			{/* Main Content */}
 			<div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-blue-400 via-indigo-500 to-purple-600 p-6 pt-20">
+				<div className="flex">
+					<img src="src/react-logo.png" alt="React Logo" className="object-cover"/>
+					<img src="src/django-logo.png" alt="Django Logo" className="object-cover"/>
+				</div>
+
 				<h1 className="text-4xl font-bold text-white mb-8 shadow-lg">Welcome to My Django/React Template</h1>
 				<Link to="/user-info">
 					<button className="px-6 py-3 text-lg font-semibold text-white bg-blue-500 rounded-lg shadow-md hover:bg-blue-600 transition duration-200 transform hover:scale-105">
